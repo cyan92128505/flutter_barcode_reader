7d6bb83ff782d303fcbe7939c24e2d4eb1f606e7
Shinku Aoma <aomashinku@outlook.com>
2019-04-08 10:28:45 +0800

    Modifed flash button text

--- ios/Classes/BarcodeScannerViewController.m
+++ ios/Classes/BarcodeScannerViewController.m
@@ -90,11 +90,11 @@
         return;
     }
     if (self.isFlashOn) {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash Off"
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"✖"
                                                                                   style:UIBarButtonItemStylePlain
                                                                                  target:self action:@selector(toggle)];
     } else {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash On"
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"⚡"
                                                                                   style:UIBarButtonItemStylePlain
                                                                                  target:self action:@selector(toggle)];
     }
